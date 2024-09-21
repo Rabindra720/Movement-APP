8cbe868b2fc76747b7bc627ea60350d823770184
Rabindra <rujandhakal12@gmail.com>
2024-09-21 20:36:51 +0300

    Asked user inputs and changed temperature of week 1

---
+++ Week 1.py
@@ -0,0 +1,18 @@
+Firstname=input("Enter your first name :")
+Lastname=input("Enter your lastname :")
+Birthdate=(input("Enter your date of birth in (dd/mm/yyyy) :"))
+fname=Firstname[0:2]
+print(type(Birthdate))
+lname=Lastname[0:3]
+birthday=Birthdate.replace("/","")
+Username=fname+lname+birthday
+print(f"Hello {Firstname}{Lastname},welcome to the Motion Detector! Let's start.Your username is {Username}. ")
+movement=input("Has there been movement in the room(YES/NO):")
+if (movement.upper()=="YES") :
+    print("Movement detected")
+else:
+    print("Movement not detected")
+temp=float(input("Enter temp in celsius:"))
+fahr = (temp * 9/5) + 32    
+print(f"The given temperature {temp}°C is {fahr:.2f} F. ")
+
